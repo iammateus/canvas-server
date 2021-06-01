61465ec07d45492d7b595d1172af679b08a7334d
Mateus Soares <mateussoaress1997@gmail.com>
2021-06-01 19:18:18 -0300

    Unit test of client repository

---
+++ tests/unit/ClientSocketRepository.test.js
@@ -0,0 +1,31 @@
+const ClientSocketRepository = require("../../app/repositories/gateways/socket.io/ClientSocketRepository");
+const faker = require("faker");
+const mockObject = require("../mocks/object.mock");
+
+describe("ClientSocketRepository.emitBroadcast", () => {
+    it("should be a function", () => {
+        const clientSocketRepository = new ClientSocketRepository();
+        expect(clientSocketRepository.emitBroadcast).toBeInstanceOf(Function);
+    });
+
+    it("should be a function", () => {
+        const client = {
+            broadcast: {
+                emit: jest.fn(),
+            },
+        };
+        const event = faker.lorem.word();
+        const data = mockObject();
+
+        const clientSocketRepository = new ClientSocketRepository(client);
+        clientSocketRepository.emitBroadcast(event, data);
+
+        expect(client.broadcast.emit.mock.calls.length).toEqual(1);
+
+        const firstArg = client.broadcast.emit.mock.calls[0][0];
+        const secondArg = client.broadcast.emit.mock.calls[0][1];
+
+        expect(firstArg).toEqual(event);
+        expect(secondArg).toMatchObject(data);
+    });
+});
