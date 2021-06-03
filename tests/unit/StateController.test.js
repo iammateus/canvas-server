451c35c040d77c9bc26e4a8e4575782845f8e4ed
Mateus Soares <mateussoaress1997@gmail.com>
2021-06-03 19:59:39 -0300

    Crate tests for state controller

--- tests/unit/StateController.test.js
+++ tests/unit/StateController.test.js
@@ -1,7 +1,23 @@
 const StateController = require("../../app/controllers/StateController");
+const ClientSocketRepository = require("../../app/repositories/gateways/socket.io/ClientSocketRepository");
+jest.mock("../../app/repositories/gateways/socket.io/ClientSocketRepository");
+const mockObject = require("../mocks/object.mock");
 
 describe("StateController.state", () => {
     it("should be a function", () => {
         expect(StateController.state).toBeInstanceOf(Function);
+    });
+
+    it("should be a function", () => {
+        const client = new ClientSocketRepository();
+        client.emitBroadcast.mockImplementationOnce(() => {});
+        const state = mockObject();
+        StateController.state(client, state);
+        expect(client.emitBroadcast.mock.calls.length).toEqual(1);
+
+        const firstArg = client.emitBroadcast.mock.calls[0][0];
+        const secondArg = client.emitBroadcast.mock.calls[0][1];
+        expect(firstArg).toEqual("state");
+        expect(secondArg).toMatchObject(state);
     });
 });
