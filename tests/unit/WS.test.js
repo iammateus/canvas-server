7883347069d09ca879f1b649433bd962d4236da9
Mateus Soares <mateussoaress1997@gmail.com>
2021-06-24 19:43:45 -0300

    Test for WS

---
+++ tests/unit/WS.test.js
@@ -0,0 +1,21 @@
+const WS = require('../../app/ports/WS');
+const StateController = require('../../app/controllers/StateController');
+const RoomController = require('../../app/controllers/RoomController');
+
+describe('WS.getEventHandlers', () => {
+    it('should be a function', () => {
+        expect(WS.getEventHandlers).toBeInstanceOf(Function);
+    });
+
+    it('should have "state" event handler', () => {
+        const handler = WS.getEventHandlers().find((item) => item.event === 'state');
+        expect(handler).toBeTruthy();
+        expect(handler.handler).toBe(StateController.state);
+    });
+
+    it('should have "room" event handler', () => {
+        const handler = WS.getEventHandlers().find((item) => item.event === 'room');
+        expect(handler).toBeTruthy();
+        expect(handler.handler).toBe(RoomController.join);
+    });
+});
