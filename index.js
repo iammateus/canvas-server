96d5c07e5862b9056ada88c04cc44d5afe4d348d
Mateus Soares <mateussoaress1997@gmail.com>
2021-07-03 16:59:45 -0300

    Index

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 const logger = require('./app/utils/logger');
-
 const SocketServer = require('./app/SocketServer');
 
 const socketServer = new SocketServer();
-
 const port = process.env.APP_PORT;
 socketServer.server.listen(port);
-
 logger.info(`App is running on port ${port}`);
